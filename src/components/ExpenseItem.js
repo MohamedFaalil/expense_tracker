bdfdee41175d1aea37f0ad7ae525a484dd95859d
mohamed-faalil <mohamedfaalil05@gmail.com>
2023-03-16 18:10:46 +0530

    removed hardcoded values in ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,14 @@
 import './ExpenseItem.css'
 function ExpenseItem() {
+    const date = new Date();
+    const currentDate = date.toLocaleDateString('');
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = "294.9";
     return (<div className="expense-item">
-        <div>March 28th 2021</div>
+        <div>{currentDate}</div>
         <div className="expense-item__description">
-            <h2>Car Insurance</h2>
-            <div className="expense-item__price">$294.9</div>
+            <h2>{expenseTitle}</h2>
+            <div className="expense-item__price">${expenseAmount}</div>
         </div>
     </div>);
 }
