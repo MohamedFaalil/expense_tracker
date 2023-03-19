0fe644d2aa9625359609d22ce13b76cfb79a3f7f
mohamed-faalil <mohamedfaalil05@gmail.com>
2023-03-19 19:34:37 +0530

    listening events practised with temp Change Tile button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,13 +3,16 @@ import './ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-
+    const clickHandler = ()=>{
+        console.log("Hey I'm triggered!");
+    };
     return (<Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
             <h2>{props.title}</h2>
             <div className="expense-item__price">${props.price}</div>
         </div>
+        <button onClick={clickHandler}>Change Title</button>
     </Card>);
 }
 
