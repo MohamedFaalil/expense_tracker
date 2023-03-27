34062854e8d8e35c761b5025d9a46d2dab00201a
mohamed-faalil <mohamedfaalil05@gmail.com>
2023-03-27 23:13:37 +0530

    removed action button on ExpenseItem component and its states. now it is controlled by its parent component. Now this called *presentation|stateless component*
    its parent component called *stateful *

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,15 @@
-import React, {useState} from "react";
+import React from "react";
 import Card from "../UI/Card";
 import './ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
-    const clickHandler = ()=>{
-        const prevTitle = props.title;
-        setTitle(`${prevTitle} (updated)`);
-    };
     return (<Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-            <h2>{title}</h2>
+            <h2>{props.title}</h2>
             <div className="expense-item__price">${props.price}</div>
         </div>
-        <button onClick={clickHandler}>Change Title</button>
     </Card>);
 }
 
