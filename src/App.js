88a7231a209ebc6f62843e64380f43065104671e
mohamed-faalil <mohamedfaalil05@gmail.com>
2023-03-28 23:08:12 +0530

    dynamically updating expenses(items) with user interaction part is done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, {useState} from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-
-const expenses = [
+const DUMMY_EXPENSES = [
     {id: 'e1', title: 'PC(Desktop)', price: 512.75, date: new Date('2023-03-14')},
     {id: 'e2', title: 'TV', price: 2985.5, date: new Date('2023-03-15')},
     {id: 'e3', title: 'Toilet Paper', price: 25.8, date: new Date('2023-03-15')},
     {id: 'e4', title: 'Car Insurance', price: 298.5, date: new Date('2023-03-16')},
     {id: 'e5', title: 'Mouse pad', price: 3, date: new Date('2023-03-17')},
 ];
+
 const App = () => {
+    const [expenses, setExpense] = useState(DUMMY_EXPENSES);
     const getNewId = () => {
         const expenseLength = expenses.length;
         const lastIdPhrase = expenses[expenseLength - 1].id;
@@ -20,6 +21,10 @@ const App = () => {
     const addExpenseHandler = (expense) => {
         expense.id = getNewId();
         console.log('new expense:', expense);
+        setExpense((prevExpenses)=>{
+            return [expense,...prevExpenses];
+        });
+        setExpense([expense, ...expenses]);
     };
 
     // OLD VERSION
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,13 +7,6 @@ import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
     const [filterDate, setFilterDate] = useState('2020');
-    let expenseItems = [];
-    props.expense_collection.forEach((expense, index) => expenseItems.push(<ExpenseItem
-        key={index}
-        title={expense.title}
-        price={expense.price}
-        date={expense.date}/>));
-
     const filterChangeHandler = (year) => {
         console.log('on expenses', year);
         setFilterDate(year);
@@ -22,7 +15,13 @@ const Expenses = (props) => {
 
         <Card className="expenses">
             <ExpensesFilter selectedYear={filterDate} onFilterChangeData={filterChangeHandler}/>
-            {expenseItems}
+            {props.expense_collection.map(expense => {
+                return <ExpenseItem
+                    key={expense.id}
+                    title={expense.title}
+                    price={expense.price}
+                    date={expense.date}/>
+            })}
         </Card>
     );
 }
