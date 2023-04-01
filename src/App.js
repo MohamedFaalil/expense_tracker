19a698815b61b2af37fa4ac58d4ca161e1f44725
mohamed-faalil <mohamedfaalil05@gmail.com>
2023-04-01 23:26:50 +0530

    dummy expenses values are updated

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const DUMMY_EXPENSES = [
-    {id: 'e1', title: 'PC(Desktop)', price: 512.75, date: new Date('2023-03-14')},
-    {id: 'e2', title: 'TV', price: 2985.5, date: new Date('2022-03-15')},
+    {id: 'e1', title: 'PC(Desktop)', price: 512.75, date: new Date('2023-02-14')},
+    {id: 'e2', title: 'TV', price: 2985.5, date: new Date('2022-02-15')},
     {id: 'e3', title: 'Toilet Paper', price: 25.8, date: new Date('2021-03-15')},
     {id: 'e4', title: 'Car Insurance', price: 298.5, date: new Date('2020-03-16')},
-    {id: 'e5', title: 'Mouse pad', price: 3, date: new Date('2020-03-17')},
+    {id: 'e5', title: 'Mouse pad', price: 3, date: new Date('2020-01-17')},
 ];
 const App = () => {
     const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
